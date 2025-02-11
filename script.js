aa0ca9fb5d69f5c2bc3e832f917bff44a11c9c72
Cumutoni12 <cumutoni@gmail.com>
2025-02-11 15:27:00 -0500

    updated

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const booksInLs=JSON.parse(localStorage.getItem("listOfBooks")) ||[]
 function updatingLocalStorage (){
     localStorage.setItem("listOfBooks",JSON.stringify(booksInLs))
 }
+// a function for showing books 
 function showBooks=(){
     const bookList=document.querySelector(".bookList")
     let books=""
@@ -18,21 +19,40 @@ function showBooks=(){
          <li>${book.author} </li>
         </ul>
 
-        <button> Remove </button>
+        <button onClick="removeBook(${index})"> Remove </button>
         <hr> 
 
         `
     })
     bookList.innerHTML=books
 }
-
+// function for adding a book 
 function addBook(bookTitle,bookAuthor){
  const book={
     title:bookTitle,
     author:bookAuthor
 
  }
+ // pushing a book in array (booksInLs)
  booksInLs.push(book)
+ alert("a book is stored in local storage")
  updatingLocalStorage()
  showBooks()
 }
+
+// function for removing the book 
+
+function removeBook(i){
+booksInLs.splice(i,1)
+updatingLocalStorage()
+showBooks()
+}
+
+const form=document.querySelector(".form")
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    const title=document.querySelector("#title").value
+    const author=document.querySelector("#author").value
+    addBook(title,author)
+})
+window.onload=showBooks()
