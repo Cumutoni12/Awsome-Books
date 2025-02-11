5fd1f8e781b79e62b6780acf25e9b4d3fab04ecc
Cumutoni12 <cumutoni@gmail.com>
2025-02-11 15:43:11 -0500

    updated

--- script.js
+++ script.js
@@ -4,16 +4,16 @@
 // const booksInLs=JSON.parse(localStorage.getItem("listOfBooks"))
 
 // retrieving data from loocal storage
-const booksInLs=JSON.parse(localStorage.getItem("listOfBooks")) ||[]
-function updatingLocalStorage (){
-    localStorage.setItem("listOfBooks",JSON.stringify(booksInLs))
+const booksInLs = JSON.parse(localStorage.getItem("listOfBooks")) || [];
+function updatingLocalStorage() {
+  localStorage.setItem("listOfBooks", JSON.stringify(booksInLs));
 }
-// a function for showing books 
-function showBooks=(){
-    const bookList=document.querySelector(".bookList")
-    let books=""
-    booksInLs.forEach((book,index)=>{
-        books+=`
+// a function for showing books
+function showBooks() {
+  const bookList = document.querySelector(".bookList");
+  let books = "";
+  booksInLs.forEach((book, index) => {
+    books += `
         <ul>
         <li> ${book.title}</li>
          <li>${book.author} </li>
@@ -22,37 +22,37 @@ function showBooks=(){
         <button onClick="removeBook(${index})"> Remove </button>
         <hr> 
 
-        `
-    })
-    bookList.innerHTML=books
+        `;
+  });
+  bookList.innerHTML = books;
 }
-// function for adding a book 
-function addBook(bookTitle,bookAuthor){
- const book={
-    title:bookTitle,
-    author:bookAuthor
-
- }
- // pushing a book in array (booksInLs)
- booksInLs.push(book)
- alert("a book is stored in local storage")
- updatingLocalStorage()
- showBooks()
-}
-
-// function for removing the book 
-
-function removeBook(i){
-booksInLs.splice(i,1)
-updatingLocalStorage()
-showBooks()
+// function for adding a book
+function addBook(bookTitle, bookAuthor) {
+  const book = {
+    title: bookTitle,
+    author: bookAuthor,
+  };
+  // pushing a book in array (booksInLs)
+  booksInLs.push(book);
+  alert("a book is stored in local storage");
+  updatingLocalStorage();
+  showBooks();
 }
 
-const form=document.querySelector(".form")
-form.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    const title=document.querySelector("#title").value
-    const author=document.querySelector("#author").value
-    addBook(title,author)
-})
-window.onload=showBooks()
+// function for removing the book
+
+function removeBook(i) {
+  booksInLs.splice(i, 1);
+  updatingLocalStorage();
+  showBooks();
+}
+
+const form = document.querySelector(".form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  addBook(title, author);
+  form.reset();
+});
+window.onload = showBooks();
